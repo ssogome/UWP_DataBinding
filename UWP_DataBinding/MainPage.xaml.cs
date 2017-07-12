be6ab6fb639641d27199eab152d1150ef3c95247
Pap Sogome <ssogome@gmail.com>
2017-07-11 19:07:01 -0600

    Add project files.

---
+++ UWP_DataBinding/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_DataBinding
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private int msDelay;
+
+        public int MsDelay
+        {
+            get
+            {
+                return msDelay;
+            }
+            private set
+            {
+                msDelay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if(button != null)
+            {
+                button.IsEnabled = false;
+                await BackgroundAction();
+
+                button.IsEnabled = true;
+            }
+        }
+
+        private Task BackgroundAction()
+        {
+            const int msDelay = 50;
+            const int iterationCount = 100;
+
+            return Task.Run(async () =>
+            {
+                for (int i = 1; i <= iterationCount; i++)
+                {
+                    await Dispatcher.RunIdleAsync((a) => { MsDelay = i; });
+
+                    Task.Delay(msDelay).Wait();
+                }
+            });
+        }
+
+        private string Sum(double val1, double val2)
+        {
+            return "sum: " + (val1 + val2).ToString();
+        }
+    }
+}
---
+++ UWP_DataBinding/NumericToMsDelayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWP_DataBinding
+{
+    public class NumericToMsDelayConverter: IValueConverter
+    {
+        private const string msSymbol = "ms";
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return string.Format("{0}  {1}", value, msSymbol);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException("Do something about the reverse conversion");
+        }
+    }
+}
